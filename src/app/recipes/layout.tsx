d57d48486f256be2fed70421a8c2049414cc14fc
Robin Schmidt <schmidtrobin02@gmail.com>
2023-11-16 18:06:59 +0100

    fix: recipe routing with tab component

--- src/app/recipes/layout.tsx
+++ src/app/recipes/layout.tsx
@@ -1,20 +1,21 @@
+"use client";
 import React from "react";
-import {Tab, Tabs} from "@nextui-org/react";
-
-export default function RecipeLayout(
-    {children}: { children: React.ReactNode}) {
-
-
-    return (
-        <main>
-            <h1>Recipe Layout</h1>
-            <Tabs>
-                <Tab key="public" title="public"/>
-
-                <Tab key="private" title="private"/>
-            </Tabs>
-            {/* Include shared UI here e.g. a header or sidebar */}
-            {children}
-        </main>
-    )
-};
+import { Tab, Tabs } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+export default function RecipeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  return (
+    <main>
+      <h1>Recipe Layout</h1>
+      <Tabs aria-label="Options" selectedKey={pathname}>
+        <Tab key="public" title="Public" href="public"></Tab>
+        <Tab key="private" title="Private" href="private"></Tab>
+      </Tabs>
+      {children}
+    </main>
+  );
+}
--- src/app/recipes/public/page.tsx
+++ src/app/recipes/public/page.tsx
@@ -1,6 +1,7 @@
 export default function Page() {
-    return (
-        <main className="flex flex-col items-center">
-            {}
-        </main>);
+  return (
+    <main className="flex flex-col items-center">
+      <h1>Public Recipes</h1>
+    </main>
+  );
 }
