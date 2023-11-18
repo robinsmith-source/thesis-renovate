26318117f5e65317814e47e4b3cfba6a2f23b236
Robin Schmidt <schmidtrobin02@gmail.com>
2023-11-18 17:22:55 +0100

    feat: add error page with custom exceptions

---
+++ src/app/error.tsx
@@ -0,0 +1,23 @@
+"use client"; // Error components must be Client Components
+
+import { Button } from "@nextui-org/react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <div className="flex flex-col items-center justify-center gap-3">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">Something went wrong!</h1>
+        <h2 className="text-xl">{error.message}</h2>
+      </div>
+      <Button variant="solid" color="warning" onClick={reset}>
+        Try Again
+      </Button>
+    </div>
+  );
+}
