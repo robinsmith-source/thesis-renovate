8ba75dacfa73570194ef018fba29381b1bc80a21
Robin Schmidt <schmidtrobin02@gmail.com>
2023-12-14 00:07:58 +0100

    refactor: improved ux and html structure

--- src/app/recipe/create/StepCreator.tsx
+++ src/app/recipe/create/StepCreator.tsx
@@ -4,6 +4,7 @@ import {
   Card,
   CardBody,
   CardHeader,
+  Divider,
   Input,
   Select,
   SelectItem,
@@ -22,36 +23,37 @@ export default function StepCreator() {
     name: "steps",
   });
   return (
-    <>
-      <div className="ml-8 space-y-4">
-        <div className="flex gap-2">
-          <div className="text-lg">Steps</div>
-          <Button
-            type="button"
-            size="sm"
-            onClick={() => append({ description: "" })}
-          >
-            Add Step
-          </Button>
-        </div>
-        {fields.map((step, index) => (
-          <motion.div
-            key={step.id}
-            layout
-            initial={{ scale: 0.8, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0.8, opacity: 0 }}
-            transition={{ type: "spring", stiffness: 500, damping: 30 }}
-          >
-            <Card>
-              <CardHeader className="flex flex-col items-start">
+    <div className="ml-8 space-y-4">
+      <div className="flex gap-2">
+        <h2 className="text-lg">Steps</h2>
+        <Button
+          type="button"
+          size="sm"
+          onClick={() => append({ description: "" })}
+        >
+          Add Step
+        </Button>
+      </div>
+      {fields.map((step, index) => (
+        <motion.div
+          key={step.id}
+          layout
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <Card>
+            <CardHeader className="flex justify-between">
+              <div className="flex items-start gap-2">
                 <Button
+                  isDisabled={index === 0}
                   isIconOnly
                   startContent={<FaChevronUp />}
                   size="sm"
                   onClick={() => index > 0 && swap(index, index - 1)}
                 />
                 <Button
+                  isDisabled={index === fields.length - 1}
                   isIconOnly
                   startContent={<FaChevronDown />}
                   size="sm"
@@ -59,93 +61,92 @@ export default function StepCreator() {
                     index < fields.length - 1 && swap(index, index + 1)
                   }
                 />
-              </CardHeader>
-              <CardBody>
-                <div className="grid grid-cols-[4fr_2fr] gap-x-2">
-                  <Controller
-                    control={control}
-                    name={`steps.${index}.description`}
-                    render={({ field, fieldState }) => (
-                      <Textarea
-                        className="row-span-2"
-                        {...field}
-                        label="Step Description"
-                        variant="bordered"
-                        isRequired
-                        isInvalid={!!fieldState.error}
-                        errorMessage={fieldState.error?.message}
-                      />
-                    )}
-                  />
-                  <Controller
-                    control={control}
-                    name={`steps.${index}.duration`}
-                    render={({ field, fieldState }) => (
-                      <Input
-                        {...field}
-                        onChange={(event) => {
-                          field.onChange(+event.target.value);
-                        }}
-                        type="number"
-                        label="Duration"
-                        variant="bordered"
-                        isRequired
-                        size="sm"
-                        isInvalid={!!fieldState.error}
-                        errorMessage={fieldState.error?.message}
-                      />
-                    )}
-                  />
+              </div>
 
-                  <Controller
-                    control={control}
-                    name={`steps.${index}.stepType`}
-                    render={({ field, fieldState }) => (
-                      <Select
-                        {...field}
-                        isRequired
-                        label="Step Type"
-                        variant="bordered"
-                        selectedKeys={[field.value]}
-                        defaultSelectedKeys={["PREP"]}
-                        size="sm"
-                        isInvalid={!!fieldState.error}
-                        errorMessage={fieldState.error?.message}
-                      >
-                        {["PREP", "COOK", "REST", "SEASON", "SERVE", "MIX"].map(
-                          (stepType) => (
-                            <SelectItem
-                              key={stepType}
-                              value={stepType as RecipeStepType}
-                              className="capitalize"
-                            >
-                              {stepType}
-                            </SelectItem>
-                          ),
-                        )}
-                      </Select>
-                    )}
-                  />
-                </div>
-                <div className="flex justify-end py-2">
-                  <Button
-                    className="place-self-stretch"
-                    color="danger"
-                    type="button"
-                    variant="flat"
-                    size="sm"
-                    onClick={() => remove(index)}
-                  >
-                    Remove Step
-                  </Button>
-                </div>
+              <Button
+                className="place-self-stretch"
+                color="danger"
+                type="button"
+                variant="flat"
+                size="sm"
+                onClick={() => remove(index)}
+              >
+                Remove Step
+              </Button>
+            </CardHeader>
+            <CardBody>
+              <div className="grid grid-cols-[4fr_2fr] gap-x-2">
+                <Controller
+                  control={control}
+                  name={`steps.${index}.description`}
+                  render={({ field, fieldState }) => (
+                    <Textarea
+                      className="row-span-2"
+                      {...field}
+                      label="Step Description"
+                      variant="bordered"
+                      isRequired
+                      isInvalid={!!fieldState.error}
+                      errorMessage={fieldState.error?.message}
+                    />
+                  )}
+                />
+                <Controller
+                  control={control}
+                  name={`steps.${index}.duration`}
+                  render={({ field, fieldState }) => (
+                    <Input
+                      {...field}
+                      onChange={(event) => {
+                        field.onChange(+event.target.value);
+                      }}
+                      type="number"
+                      label="Duration"
+                      variant="bordered"
+                      isRequired
+                      size="sm"
+                      isInvalid={!!fieldState.error}
+                      errorMessage={fieldState.error?.message}
+                    />
+                  )}
+                />
 
-                <IngredientCreator stepIndex={index} />
-              </CardBody>
-            </Card>
-          </motion.div>
-        ))}
-      </div>
-    </>
+                <Controller
+                  control={control}
+                  name={`steps.${index}.stepType`}
+                  render={({ field, fieldState }) => (
+                    <Select
+                      {...field}
+                      isRequired
+                      label="Step Type"
+                      variant="bordered"
+                      selectedKeys={[field.value]}
+                      defaultSelectedKeys={["PREP"]}
+                      size="sm"
+                      isInvalid={!!fieldState.error}
+                      errorMessage={fieldState.error?.message}
+                    >
+                      {["PREP", "COOK", "REST", "SEASON", "SERVE", "MIX"].map(
+                        (stepType) => (
+                          <SelectItem
+                            key={stepType}
+                            value={stepType as RecipeStepType}
+                            className="capitalize"
+                          >
+                            {stepType}
+                          </SelectItem>
+                        ),
+                      )}
+                    </Select>
+                  )}
+                />
+              </div>
+              <Divider className="my-4" />
+              <IngredientCreator stepIndex={index} />
+            </CardBody>
+          </Card>
+        </motion.div>
+      ))}
+    </div>
   );
 }
