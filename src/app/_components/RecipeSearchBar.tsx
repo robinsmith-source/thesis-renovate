249bccf54dfc694c4aef50d5d446670287f9b532
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2023-12-15 23:21:57 +0100

    fix: redesigned searchbar component

--- src/app/_components/RecipeSearchBar.tsx
+++ src/app/_components/RecipeSearchBar.tsx
@@ -1,27 +1,47 @@
-"use client";
-
-import { useState } from "react";
+import { useState, type KeyboardEvent } from "react";
 import { Input } from "@nextui-org/react";
 import { SearchIcon } from "@nextui-org/shared-icons";
+import { type SubmitHandler } from "react-hook-form";
+import { useRouter, useSearchParams } from "next/navigation";
 
-export default function RecipeSearchBar({ onSearch }) {
-  const [searchQuery, setSearchQuery] = useState("");
+interface FormValues {
+  search: string;
+}
 
-  const handleSearch = () => {
-    onSearch(searchQuery);
+const SearchInput = () => {
+  const search = useSearchParams();
+  const [searchQuery, setSearchQuery] = useState<string | null>(
+    search ? search.get("q") : "",
+  );
+  const router = useRouter();
+
+  const onSearch: SubmitHandler<FormValues> = (data) => {
+    const encodedSearchQuery = encodeURI(data.search);
+    router.push(`explore/search?q=${encodedSearchQuery}`);
+    console.log("searching for:  " + data.search);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onSearch({ search: searchQuery ?? "" });
+    }
   };
 
   return (
     <Input
       fullWidth
       type="text"
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-      onKeyDown={(e) => {
-        if (e.key === "Enter") handleSearch();
-      }}
+      value={searchQuery ?? ""}
+      onChange={(event) => setSearchQuery(event.target.value)}
+      onKeyDown={handleKeyDown}
       placeholder="Search recipes..."
-      endContent={<SearchIcon name="search" onClick={handleSearch} />}
+      endContent={
+        <SearchIcon
+          name="search"
+          onClick={() => onSearch({ search: searchQuery ?? "" })}
+        />
+      }
     />
   );
-}
+};
+export default SearchInput;
