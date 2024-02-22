8fc4584328e9985de00a3a9f8548fbae0f4fb61b
Tobias Messner <tobias@zusor.io>
2024-02-22 18:02:42 +0100

    feat: add more user metadata

--- src/app/user/[id]/layout.tsx
+++ src/app/user/[id]/layout.tsx
@@ -1,18 +1,28 @@
 import { Divider } from "@nextui-org/react";
 import { notFound } from "next/navigation";
+import { cache } from "react";
 import UserCard from "~/app/_components/UserCard";
 import { api } from "~/trpc/server";
-import { cache } from "react";
 
 const getUser = cache(async (id: string) => {
   return await api.user.get.query({ id });
 });
 
+const getUserMeta = cache(async (id: string) => {
+  return await api.user.getMetadata.query({ id });
+});
+
 export async function generateMetadata({ params }: { params: { id: string } }) {
-  const recipe = await getUser(params.id);
+  const user = await getUser(params.id);
+  const meta = await getUserMeta(params.id);
 
   return {
-    title: recipe.name,
+    title: user.name,
+    description: `They've created ${meta.recipeCount} ${
+      meta.recipeCount === 1 ? "recipe" : "recipes"
+    }, are followed by ${meta.followedByCount} people, and follow ${
+      meta.followingCount
+    } people.`,
   };
 }
 
